c052850b3ecebc3f41aaa17e7ffc854d578ab838
DarrenAscione <darren_ng@mymail.sutd.edu.sg>
2018-05-27 17:29:20 +0800

    added unit test for util func

--- src/main/common/util.py
+++ src/main/common/util.py
@@ -8,8 +8,8 @@ def mean(alist):
 
 def std(alist):
 	avg = mean(alist)
-	diff = [math.pow(num - avg, 2) for num in alist]
-	return math.pow((1.0/(len(alist)))*sum(diff), 0.5)
+	diff = [pow(num - avg, 2) for num in alist]
+	return pow((1.0/(len(alist)))*sum(diff), 0.5)
 
 def mad(alist):
 	avg = mean(alist)
@@ -19,11 +19,12 @@ def mad(alist):
 def skewness(alist):
 	avg = mean(alist)
 	stand_dev = std(alist)
-	diff = [math.pow((num - avg)/stand_dev,3)*1.0 for num in alist]
+	diff = [pow((num - avg)/stand_dev,3)*1.0 for num in alist]
 	return sum(diff)/len(alist)
 
 def kurtosis(alist):
 	avg = mean(alist)
 	stand_dev = std(alist)
-	diff = [math.pow((num - avg)/stand_dev,4)*1.0 for num in alist]
+	diff = [pow((num - avg)/stand_dev,4)*1.0 for num in alist]
 	return (sum(diff)/len(alist))-3
+
--- src/test/common/test_util.py
+++ src/test/common/test_util.py
@@ -3,10 +3,31 @@ import util
 
 class TestUtil(unittest.TestCase):
 
+	global alist
+	alist = [1,2,3,4,5]
+
 	def test_mean(self):
-		alist = [1,2,3,4,5]
 		result = util.mean(alist)
-		self.assertEquals(result, 3)
+		self.assertEquals(result, 3.0)
+
+	def test_std(self):
+		result = round(util.std(alist),2)
+		self.assertEquals(result, 1.41)
+
+	def test_mad(self):
+		result = util.mad(alist)
+		self.assertEquals(result, 1.2)
+
+	def test_skewness(self):
+		result = util.skewness(alist)
+		self.assertEquals(result, 0.0)
+
+	def test_kurtosis(self):
+		result = round(util.kurtosis(alist),2)
+		self.assertEquals(result, -1.3)
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
