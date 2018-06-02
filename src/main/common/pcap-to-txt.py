3b57162b84d3833763f42d0a958c7d756bb04d4e
DarrenAscione <darren_ng@mymail.sutd.edu.sg>
2018-06-02 17:42:00 +0800

    added pcap convertor to txt

---
+++ src/main/common/pcap-to-txt.py
@@ -0,0 +1,8 @@
+import os
+
+def convert(file_in, file_out, conditon = ""):
+	print("Converting pcap file " + file_in + "...")
+	os.system('tshark -r ../../../data/%s -Y "%s" -T fields -e ip.src -e ip.dst -e frame.number -e _ws.col.Info -E header=y > ../../../data/%s' %(file_in, conditon, file_out))
+	print("..\n Successfully converted PCAP into %s" %(file_out))
+
+#convertor("cipKiller40-50.pcap", "test.txt")
